b7c75cddb62f9d01d582582674241831f3948f18
Dave Rapin <dave@rapin.com>
2025-10-27 10:41:51 -0400

    Added a link to curlingschedules.com in the footer.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -130,6 +130,10 @@ module.exports = {
               label: 'Privacy',
               to: 'privacy',
             },
+            {
+              label: 'Curling Schedules',
+              href: 'https://curlingschedules.com',
+            },
           ],
         },
       ],
